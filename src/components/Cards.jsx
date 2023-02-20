6292b4a618af47cb54d31974b0f52e10685e3e55
Diego101101 <111402891+Daigoares@users.noreply.github.com>
2023-02-19 22:16:21 -0500

    01

---
+++ src/components/Cards.jsx
@@ -0,0 +1,19 @@
+import Card from './Card';
+
+export default function Cards(props) {
+   const { characters } = props;
+   return <div>
+            {
+            characters.map((character)=> {
+               return <Card
+                  key = {character.id}
+                  name = {character.name}
+                  species ={character.species}
+                  gender = {character.gender}
+                  image = {character.image}
+                  onClose = {() => window.alert('Emulamos que se cierra la card')}
+               />
+            } )
+            } 
+          </div>;
+}
