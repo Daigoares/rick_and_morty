1e74709466eb66dab6bfa24a36ecc7f81deb2f53
Diego101101 <111402891+Daigoares@users.noreply.github.com>
2023-02-21 16:11:48 -0500

    01.1

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import Card from './components/Card.jsx'
 import Cards from './components/Cards.jsx'
 import SearchBar from './components/SearchBar.jsx'
 import characters, { Rick } from './data.js'
+import logo from './img/logo1.jpg'
 
+ 
 function App () {
   return (
     <div className='App' style={{ padding: '25px' }}>
@@ -16,13 +18,13 @@ function App () {
           onClose={() => window.alert('Emulamos que se cierra la card')}
         />
       </div>
-      <hr />
+      
       <div>
         <Cards
           characters={characters}
         />
       </div>
-      <hr />
+    
       <div>
         <SearchBar
           onSearch={(characterID) => window.alert(characterID)}
